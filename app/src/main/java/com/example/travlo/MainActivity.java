36c0027947d3aeb55e904ab132873d3ad5e0fce2
Omkar Kumbhar <om.kumbhar1998@gmail.com>
2019-09-02 03:39:00 +0530

    second commit

---
+++ app/src/main/java/com/example/travlo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.travlo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(! prefs.getBoolean("firstRun",false)){
+            Intent i =new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(i);
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstRun", true);
+            editor.apply();
+            finish();
+        }
+    }
+
+
+}
