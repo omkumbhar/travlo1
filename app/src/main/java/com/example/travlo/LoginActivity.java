57b0d9c62e1bfb492360d4f32f9b5edc09fee817
omkumbhar1998 <44564574+omkumbhar1998@users.noreply.github.com>
2019-09-02 13:35:25 +0530

    Update LoginActivity.java

--- app/src/main/java/com/example/travlo/LoginActivity.java
+++ app/src/main/java/com/example/travlo/LoginActivity.java
@@ -12,6 +12,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toast.makeText(this, "hiiii", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "second HII", Toast.LENGTH_SHORT).show();
 
     }
 }
