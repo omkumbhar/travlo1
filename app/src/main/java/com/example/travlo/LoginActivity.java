92f08af099ebcf7d9788443c6fc49cb13555262b
Omkar Kumbhar <om.kumbhar1998@gmail.com>
2019-09-02 20:44:48 +0530

    login buttons add

--- app/src/main/java/com/example/travlo/LoginActivity.java
+++ app/src/main/java/com/example/travlo/LoginActivity.java
@@ -3,16 +3,39 @@ package com.example.travlo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.SignInButton;
+
 public class LoginActivity extends AppCompatActivity {
+    SignInButton gBtn;
+    Button fBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toast.makeText(this, "hiiii", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "second HII", Toast.LENGTH_SHORT).show();
+        gBtn = findViewById(R.id.google_sign_in_button);
+        fBtn = findViewById(R.id.fBtn);
+
+        gBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Google btn", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Facebook btn", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
     }
 }
